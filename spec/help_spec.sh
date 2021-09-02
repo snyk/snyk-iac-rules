a6f08d1a2c210d2e957cbc8cc2fa1d23f9f968b3
Teodora Sandu <teodora.sandu@snyk.io>
2021-09-02 13:55:31 +0100

    feat: implement test command

--- spec/help_spec.sh
+++ spec/help_spec.sh
@@ -3,6 +3,19 @@ Describe 'go run main.go'
    It 'returns help info'
       When call go run main.go
       The status should be success
-      The output should include 'An SDK to write, debug, test, and bundle custom rules for Snyk IaC.'
+      The output should include 'An SDK to write, debug, test, and bundle custom rules for Snyk IaC.
+
+Usage:
+  snyk-iac-custom-rules [command]
+
+Available Commands:
+  completion  generate the autocompletion script for the specified shell
+  help        Help about any command
+  test        Execute Rego test cases
+
+Flags:
+  -h, --help   help for snyk-iac-custom-rules
+
+Use "snyk-iac-custom-rules [command] --help" for more information about a command.'
    End
 End
---
+++ util/term.go
@@ -0,0 +1,30 @@
+package util
+
+import (
+	"os"
+	"strings"
+
+	"github.com/open-policy-agent/opa/ast"
+	"github.com/open-policy-agent/opa/version"
+)
+
+// Term returns the runtime information as an ast.Term object.
+func Term() (*ast.Term, error) {
+	obj := ast.NewObject()
+	env := ast.NewObject()
+
+	for _, s := range os.Environ() {
+		parts := strings.SplitN(s, "=", 2)
+		if len(parts) == 1 {
+			env.Insert(ast.StringTerm(parts[0]), ast.NullTerm())
+		} else if len(parts) > 1 {
+			env.Insert(ast.StringTerm(parts[0]), ast.StringTerm(parts[1]))
+		}
+	}
+
+	obj.Insert(ast.StringTerm("env"), ast.NewTerm(env))
+	obj.Insert(ast.StringTerm("version"), ast.StringTerm(version.Version))
+	obj.Insert(ast.StringTerm("commit"), ast.StringTerm(version.Vcs))
+
+	return ast.NewTerm(obj), nil
+}
