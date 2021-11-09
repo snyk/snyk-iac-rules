9b66a99b7d41e4300bd4201d2534083c41c1e4ea
Automated Release <teodora.sandu@snyk.io>
2021-11-09 18:24:14 +0000

    fix: speed up template command by removing added sleep

--- spec/e2e/template_spec.sh
+++ spec/e2e/template_spec.sh
@@ -22,13 +22,13 @@ Describe './snyk-iac-rules template ./fixtures/custom-rules --rule test'
    It 'returns passing test status'
       When call ./snyk-iac-rules template ./fixtures/custom-rules --rule test
       The status should be success
-      The stderr should include 'Template rules directory'
-      The stderr should include 'Template rules/test directory'
-      The stderr should include 'Template rules/test/main.rego file'
-      The stderr should include 'Template rules/test/main_test.rego file'
-      The stderr should include 'Template rules/test/fixtures directory'
-      The stderr should include 'Template rules/test/fixtures/allowed.tf file'
-      The stderr should include 'Template rules/test/fixtures/denied.tf file'
+      The output should include 'Template rules directory'
+      The output should include 'Template rules/test directory'
+      The output should include 'Template rules/test/main.rego file'
+      The output should include 'Template rules/test/main_test.rego file'
+      The output should include 'Template rules/test/fixtures directory'
+      The output should include 'Template rules/test/fixtures/allowed.tf file'
+      The output should include 'Template rules/test/fixtures/denied.tf file'
       The output should include 'Generated template'
    End
 End
